5769cee207118470278e788ff7f1c73b78277b98
justint <justintcoding@gmail.com>
2017-10-07 12:24:16 -0400

    Remove unnecessary directories

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 // Node modules
 const bodyParser   = require('body-parser');
-const cookieParser = require('cookie-parser');
+const botly        = require('botly');
 const express	   = require('express');
 const session	   = require('express-session');
-const flash        = require("connect-flash");
 // Custom Middleware
 const passport	   = require('./auth/');
-const configRoutes = require('./routes');
-const exhbs		   = require('./views');
+const configRoutes = require('./routes/');
+
+const fb_bot = new Botly({
+
+});
 
 let app = express();
 // Log incoming requests
@@ -16,21 +18,8 @@ app.use((req, res, next) => {
 	next();
 });
 // Parse cookies and POST body
-app.use(cookieParser());
 app.use(bodyParser.json({limit: '5mb'}));
 app.use(bodyParser.urlencoded({ limit: '5mb', extended: true }));
-// Enable Cookie session
-app.use(session({
-	secret: 'some complex secret',
-	resave: false,
-	saveUninitialized: false,
-	cookie: { maxAge : 3600000 },
-}));
-// Enable Passport middleware
-app.use(passport.initialize());
-app.use(passport.session());
-// Add flash support
-app.use(flash());
 // Serve static files
 exhbs(app, express.static(__dirname + '/public'));
 // API routing
